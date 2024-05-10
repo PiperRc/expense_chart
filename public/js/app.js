380d5320116791545408a5b9f5dfbd2527fbbcf0
Cheyenne Piper <robeltopiper@gmail.com>
2024-05-10 01:44:52 -0400

    Build bars

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,33 @@
-fetch("data.json") 
-.then((res)=>{ 
-return res.json(); 
-}) 
-.then((data)=>{ 
-return data[0].day
-})
+// const ctx = document.getElementById('myChart');
+
+// new Chart(ctx, {
+//   type: 'bar',
+//   data: {
+//     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//     datasets: [{
+//       label: '# of Votes',
+//       data: [12, 19, 3, 5, 2, 3],
+//       borderWidth: 1
+//     }]
+//   },
+//   options: {
+//     scales: {
+//       y: {
+//        display:false
+//       },
+//       x:{
+//         grid:{
+//           display:false
+//         }
+//       }
+//     }
+//   }
+// });
+
+// // fetch("data.json") 
+// // .then((res)=>{ 
+// // return res.json(); 
+// // }) 
+// // .then((data)=>{ 
+// // div.innerHTML=data[0].day
+// // })
